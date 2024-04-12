aa6bbfaa142997eb1073b3f4994d52a07c3f48a7
Mark Millard <msm@wizzerworks.com>
2024-04-12 16:55:23 -0600

    Tweaking installation script to remove sudo dependency.

--- util/mac/build/mlutil/install-mlutil.sh
+++ util/mac/build/mlutil/install-mlutil.sh
@@ -4,22 +4,26 @@ if [ -d "/usr/local/lib" ]
 then
     echo "Directory /usr/local/lib exists." 
 else
-    
-    echo "Warning: Directory /usr/local/lib does not exist. Creating it."
-    sudo mkdir /usr/local/lib
+    #echo "Warning: Directory /usr/local/lib does not exist. Creating it."
+    #sudo mkdir /usr/local/lib
+
+    echo "Error: Directory /usr/local/lib does not exist."
+    exit 1
 fi
 
 echo "Installing libmlutil.dylib to /usr/local/lib."
-sudo cp ./mlutil-DerivedData/mlutil/Build/Products/Debug/libmlutil.dylib /usr/local/lib
+cp ./mlutil-DerivedData/mlutil/Build/Products/Debug/libmlutil.dylib /usr/local/lib
 
 if [ -d "/usr/local/include" ] 
 then
     echo "Directory /usr/local/include exists." 
 else
-    
-    echo "Warning: Directory /usr/local/include does not exist. Creating it."
-    sudo mkdir /usr/local/include
+    #echo "Warning: Directory /usr/local/include does not exist. Creating it."
+    #sudo mkdir /usr/local/include
+        
+    echo "Error: Directory /usr/local/include does not existt."
+    exit 1
 fi
 
 echo "Installing libmlutil header files to /usr/local/include."
-sudo cp -R ./mlutil-DerivedData/mlutil/Build/Products/Debug/usr/local/include/* /usr/local/include
+cp -R ./mlutil-DerivedData/mlutil/Build/Products/Debug/usr/local/include/* /usr/local/include
